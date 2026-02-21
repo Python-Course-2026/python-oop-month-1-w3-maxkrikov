e119a9241633c74a1b802d85882a32eea3405543
Max <36303304+maxkrikov@users.noreply.github.com>
2026-02-21 14:48:10 +0300

    Update smart_point.py

--- week_intensiv/day1_magic/tasks/smart_point.py
+++ week_intensiv/day1_magic/tasks/smart_point.py
@@ -2,6 +2,6 @@ class SmartPoint:
     """ЗАДАЧА: Реализовать __str__ (Точка(x, y)) и __repr__ (SmartPoint(x=x, y=y))"""
     def __init__(self, x, y): self.x, self.y = x, y
     def __str__(self):
-        return f"(Точка({self.x), {self.y}))"
+        return f"(Точка({self.x}, {self.y}))"
     def __repr__(self):
         return f"SmartPoint(x={self.x}, y={self.y})"
