7f6872b21b8454de0ac65818191d4f184ad0d3bc
Max <36303304+maxkrikov@users.noreply.github.com>
2026-02-21 14:56:22 +0300

    Update wallet.py

--- week_intensiv/day1_magic/tasks/wallet.py
+++ week_intensiv/day1_magic/tasks/wallet.py
@@ -2,6 +2,6 @@ class Wallet:
     """ЗАДАЧА: Сложение кошельков через __add__ (новый Wallet) и длина через __len__ (целый баланс)"""
     def __init__(self, name, balance): self.name, self.balance = name, balance
     def __add__(self, other):
-        return Wallet(self.balance + other.balance)
+        return Wallet("Joint Account", self.balance + other.balance)
     def __len__(self):
         return int(self.balance)
