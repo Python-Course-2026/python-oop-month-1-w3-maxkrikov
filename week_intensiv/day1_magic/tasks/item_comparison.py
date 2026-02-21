13f9f851bfb16dcb3f5a0a953a6a937a411437b6
Max <36303304+maxkrikov@users.noreply.github.com>
2026-02-21 14:33:18 +0300

    Update item_comparison.py

--- week_intensiv/day1_magic/tasks/item_comparison.py
+++ week_intensiv/day1_magic/tasks/item_comparison.py
@@ -1,5 +1,11 @@
 class Item:
     """ЗАДАЧА: Сравнение товаров по цене через __lt__ и по всем полям через __eq__"""
-    def __init__(self, name, price): self.name, self.price = name, price
-    def __lt__(self, other): pass
-    def __eq__(self, other): pass
+
+    def __init__(self, name, price):
+        self.name, self.price = name, price
+
+    def __lt__(self, other):
+        return self.price < other
+
+    def __eq__(self, other):
+        return self.price == other
